4536390189b1e6812285b5c79ae5c66fb088cd2d
Alexander Fakeev <fakeev@gmail.com>
2023-04-22 11:04:19 +0300

    Minor changes

--- SparseLinearSolver/Source/main.cpp
+++ SparseLinearSolver/Source/main.cpp
@@ -1,55 +1,14 @@
-/*******************************************************************************
-* Copyright 2004-2022 Intel Corporation.
-*
-* This software and the related documents are Intel copyrighted  materials,  and
-* your use of  them is  governed by the  express license  under which  they were
-* provided to you (License).  Unless the License provides otherwise, you may not
-* use, modify, copy, publish, distribute,  disclose or transmit this software or
-* the related documents without Intel's prior written permission.
-*
-* This software and the related documents  are provided as  is,  with no express
-* or implied  warranties,  other  than those  that are  expressly stated  in the
-* License.
-*******************************************************************************/
 
-/*
-*   Content : Intel(R) oneAPI Math Kernel Library (oneMKL) PARDISO C example
-*
-********************************************************************************
-*/
-/* -------------------------------------------------------------------- */
-/* Example program to show the use of the "PARDISO" routine */
-/* on symmetric linear systems */
-/* -------------------------------------------------------------------- */
-/* This program can be downloaded from the following site: */
-/* www.pardiso-project.org */
-/* */
-/* (C) Olaf Schenk, Department of Computer Science, */
-/* University of Basel, Switzerland. */
-/* Email: olaf.schenk@unibas.ch */
-/* -------------------------------------------------------------------- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-#include "mkl_pardiso.h"
-#include "mkl_types.h"
 #include <iostream>
 #include <fstream>
 
-// Define the format to printf MKL_INT values
-#if !defined(MKL_ILP64)
-#define IFORMAT "%i"
-#else
-#define IFORMAT "%lli"
-#endif
 
 int main (void)
 {
     /* Matrix data. */
-    MKL_INT n = 8;
-    MKL_INT ia[9] = { 1, 5, 8, 10, 12, 15, 17, 18, 19};
-    MKL_INT ja[18] = 
+    int n = 8;
+    int ia[9] = { 1, 5, 8, 10, 12, 15, 17, 18, 19};
+    int ja[18] =
     { 1,    3,       6, 7,
          2, 3,    5,
             3,             8,
